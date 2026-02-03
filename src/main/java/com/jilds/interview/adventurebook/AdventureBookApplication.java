5e4a5ecc03f0588dd6d8ebcf9a7d5ecfce8d47b7
Jonathan Lima <jilds@outlook.com>
2026-02-03 20:55:13 +0000

    Initial commit

---
+++ src/main/java/com/jilds/interview/adventurebook/AdventureBookApplication.java
@@ -0,0 +1,13 @@
+package com.jilds.interview.adventurebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventureBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventureBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jilds/interview/adventurebook/AdventureBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jilds.interview.adventurebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventureBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
